5a5cba0c782b69ac305c1f5ce78fb6ccb4bc9858
apesteguia <mikeltxomikeltxo@gmail.com>
2023-03-12 23:49:50 +0100

    adding optimization

--- optimization.lua
+++ optimization.lua
@@ -1,7 +1,15 @@
 function deleteEnemies()
 	for i, v in ipairs(enemies) do
-		if v.y == 200 then
+		if v.y > 600 then
 			table.remove(enemies, i)
 		end
 	end
+end
+
+function deleteStars()
+    for i, v in ipairs(stars) do
+        if stars[i][2] > 600 then
+            table.remove(stars, i)
+        end
+    end
 end
